a53a8c52ef8ab1af3e173a45b317d9300d7e4e7b
Gustavo Peregrino <gustavo.peregrino@estudante.ufjf.br>
2025-09-25 21:40:12 -0300

    Adicona estilo de disciplina

---
+++ src/assets/Disciplina.tsx
@@ -0,0 +1,13 @@
+
+import "./Disciplina.css";
+
+function Disciplina(){
+    return (
+        <div className="disciplina">
+            <h1>DCC207</h1>
+            <h2>Desenvolvimento Web Front-End</h2>
+        </div>
+    );
+}
+
+export default Disciplina;
