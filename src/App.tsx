ec0277642dd6703fe93e12d3d828c3e1f613f438
Gustavo Peregrino <gustavo.peregrino@estudante.ufjf.br>
2025-09-25 22:25:45 -0300

    Ultimo commit aula dia25

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Disciplina from "./Disciplina";
 
 function App(){
-  return <h1>Hello, world"</h1>
+  return (
+    <>
+    <h1>Olá, mundo</h1>
+    <h2>Olá react</h2>
+    <Disciplina codigo="DCC202" nome="Desenvolvimento web I" />
+    <Disciplina codigo="DCC206" nome="Desenvolvimento web II" />
+    <Disciplina codigo="Dcc207" nome="Desenvolvimento web Front-End" />
+    </>
+
+  )
 }
 
 export default App;
---
+++ src/Disciplina.tsx
@@ -0,0 +1,18 @@
+
+import "./Disciplina.css";
+
+type DisciplinaProps = {
+    codigo: string;
+    nome: string;
+};
+
+function Disciplina({codigo, nome}: DisciplinaProps){
+    return (
+        <div className="disciplina">
+            <h1>{codigo}</h1>
+            <h2>{nome}</h2>
+        </div>
+    );
+}
+
+export default Disciplina;
